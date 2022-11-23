10b0ca8edbf67b987470c01cc940cdc25565d796
Ian <istewart17@my.bcit.ca>
2022-11-23 13:50:14 -0800

    practice adding and removing attributes

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,25 @@
-const clickMeButton = document.querySelector(".click-me");
-// console.log(clickMeButton);
-function clickHandler() {
-    alert("You clicked me!");
-    // clickMeButton.removeEventListener("click", clickHandler);
-};
-function backgroundPink() {
-    document.body.classList.add("pinkBackground")
-};
+// const clickMeButton = document.querySelector(".click-me");
 
-clickMeButton.addEventListener("click", clickHandler, {once:true})
-clickMeButton.addEventListener("click", backgroundPink)
+// function clickHandler() {
+//     alert("You clicked me!");
+ 
+// };
+// function backgroundPink() {
+//     document.body.classList.add("pinkBackground")
+// };
+
+// clickMeButton.addEventListener("click", clickHandler, {once:true})
+// clickMeButton.addEventListener("click", backgroundPink)
+// clickMeButton.addEventListener("click", updateImage)
+
+
+// function updateImage(e) {
+//     const image = document.querySelector("#shoppingCart");
+//     image.removeAttribute("hidden")
+//     image.height = 50
+//     image.width = 50
+//     console.log(e.target)
+
+// }   
+
 
